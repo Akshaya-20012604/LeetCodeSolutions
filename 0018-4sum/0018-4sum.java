a2112e031b33698b005394d07ab39333bef8fae8
Akshaya-20012604 <akshayanm26@gmail.com>
2026-02-01 22:21:40 +0530

    Time: 22 ms (23.3%), Space: 46 MB (29.71%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -8,11 +8,11 @@ class Solution {
         Arrays.sort(nums);
 
         for (int i = 0; i < n - 3; i++) {
-            // Skip duplicate i
+            
             if (i > 0 && nums[i] == nums[i - 1]) continue;
 
             for (int j = i + 1; j < n - 2; j++) {
-                // Skip duplicate j
+             
                 if (j > i + 1 && nums[j] == nums[j - 1]) continue;
 
                 int left = j + 1;
@@ -26,7 +26,7 @@ class Solution {
                                 nums[i], nums[j], nums[left], nums[right]
                         ));
 
-                        // Skip duplicates for left and right
+                        
                         while (left < right && nums[left] == nums[left + 1]) left++;
                         while (left < right && nums[right] == nums[right - 1]) right--;
 
