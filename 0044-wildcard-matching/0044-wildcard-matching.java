915ddc6308dd3723f45886b263e9223e905093ba
Akshaya-20012604 <akshayanm26@gmail.com>
2026-02-05 20:22:56 +0530

    Time: 2 ms (93.61%), Space: 45.4 MB (87.13%) - LeetHub

---
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -0,0 +1,38 @@
+class Solution {
+    public boolean isMatch(String s, String p) {
+        int i = 0; 
+        int j = 0; 
+
+        int starIdx = -1;   
+        int matchIdx = -1;  
+        while (i < s.length()) {
+
+            if (j < p.length() && (p.charAt(j) == s.charAt(i) || p.charAt(j) == '?')) {
+                i++;
+                j++;
+            }
+
+            else if (j < p.length() && p.charAt(j) == '*') {
+                starIdx = j;
+                matchIdx = i;
+                j++;
+            }
+
+            else if (starIdx != -1) {
+                j = starIdx + 1;
+                matchIdx++;
+                i = matchIdx;
+            }
+
+            else {
+                return false;
+            }
+        }
+
+        while (j < p.length() && p.charAt(j) == '*') {
+            j++;
+        }
+
+        return j == p.length();
+    }
+}
