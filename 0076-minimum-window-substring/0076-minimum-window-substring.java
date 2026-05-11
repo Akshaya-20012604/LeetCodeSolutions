1bbd58dbc6366d50259b73a6bcd82884a7a535d6
Akshaya-20012604 <akshayanm26@gmail.com>
2026-05-11 23:17:19 +0530

    Time: 20 ms (39.97%), Space: 47.6 MB (35.03%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -2,13 +2,20 @@ class Solution {
     public String minWindow(String s, String t) {
         if (s.length() < t.length()) return "";
 
-        int[] need = new int[128]; 
+        Map<Character, Integer> need = new HashMap<>();
+
+        // Count frequency of chars in t
         for (char c : t.toCharArray()) {
-            need[c]++;
+            need.put(c, need.getOrDefault(c, 0) + 1);
         }
 
-        int left = 0, right = 0;
-        int required = t.length();
+        int left = 0;
+        int right = 0;
+
+        int required = need.size();
+        int formed = 0;
+
+        Map<Character, Integer> window = new HashMap<>();
 
         int minLen = Integer.MAX_VALUE;
         int start = 0;
@@ -16,28 +23,41 @@ class Solution {
         while (right < s.length()) {
             char c = s.charAt(right);
 
-            if (need[c] > 0) {
-                required--;
-            }
-            need[c]--;
-            right++;
+            window.put(c, window.getOrDefault(c, 0) + 1);
 
-            while (required == 0) {
-                if (right - left < minLen) {
-                    minLen = right - left;
+            // Check if current char frequency matches required frequency
+            if (need.containsKey(c) &&
+                window.get(c).intValue() == need.get(c).intValue()) {
+                formed++;
+            }
+
+            // Try shrinking window
+            while (left <= right && formed == required) {
+
+                // Update minimum window
+                if (right - left + 1 < minLen) {
+                    minLen = right - left + 1;
                     start = left;
                 }
 
-                char leftChar = s.charAt(left);
-                need[leftChar]++;
+                char ch = s.charAt(left);
 
-                if (need[leftChar] > 0) {
-                    required++;
+                window.put(ch, window.get(ch) - 1);
+
+                if (need.containsKey(ch) &&
+                    window.get(ch) < need.get(ch)) {
+                    formed--;
                 }
+
                 left++;
             }
+
+            right++;
         }
 
-        return minLen == Integer.MAX_VALUE ? "" : s.substring(start, start + minLen);
+        return minLen == Integer.MAX_VALUE
+                ? ""
+                : s.substring(start, start + minLen);
+    
     }
 }
